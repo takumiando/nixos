5578a3852d58d991a14ffa2427038919f61849c4
Takumi Ando <me@tmmyando.com>
2025-01-16 17:05:08 +0900

    update.sh: Run only nixos-rebuild as root
    Signed-off-by: Takumi Ando <me@tmmyando.com>

--- update.sh
+++ update.sh
@@ -2,18 +2,13 @@
 
 cd "$(dirname "$0")" || exit 1
 
-if [ "$(whoami)" != root ]; then
-    printf "Please run as root\n"
-    exit 1
-fi
-
 HOST="$(hostname)"
 if [ -n "$1" ]; then
     HOST="$1"
 fi
 
 if [ -f ./modules/hosts/"$HOST".nix ]; then
-    nixos-rebuild switch --flake .#"$HOST"
+    sudo nixos-rebuild switch --flake .#"$HOST"
 fi
 
 home-manager switch --flake .#takumi
