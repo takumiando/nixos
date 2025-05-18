f91da8eb9005c553e73d4f87e5c4ee5a1b7b36db
Takumi Ando <me@tmmyando.com>
2025-05-18 19:31:09 +0900

    ramona: Update for the real machine
    Signed-off-by: Takumi Ando <me@tmmyando.com>

--- modules/hosts/ramona.nix
+++ modules/hosts/ramona.nix
@@ -4,7 +4,11 @@
   imports =
     [
       ./hardware/ramona.nix
+      ./options/tpkbd.nix
     ];
 
   networking.hostName = "ramona";
+
+  # Enable fingerprint reader
+  services.fprintd.enable = true;
 }
