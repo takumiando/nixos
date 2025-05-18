5e3b95046e2079ab7bb9cfae8cf4f1e3e27144c1
Takumi Ando <me@tmmyando.com>
2025-05-18 21:07:08 +0900

    update.sh: Fix date command
    Signed-off-by: Takumi Ando <me@tmmyando.com>

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ fi
 nix flake update
 
 if [ -n "$(git diff -- flake.lock)" ]; then
-    DATETIME="$(date -s '+%Y-%m-%d %H:%M:%S')"
+    DATETIME="$(date '+%Y-%m-%d %H:%M:%S')"
     git add flake.lock
     git commit --signoff -m "flake.lock: Update $DATETIME"
 fi
