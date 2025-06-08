c21cb5d4484f5ca882b3b16209c9ffc72f587b87
Takumi Ando <takumi@spacecubics.com>
2025-06-08 23:25:36 +0900

    common: Enable virtualization
    Signed-off-by: Takumi Ando <takumi@spacecubics.com>

--- modules/common.nix
+++ modules/common.nix
@@ -116,6 +116,23 @@
     };
   };
 
+  # Enable libvirt
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
   # Enable fwupd
   services.fwupd.enable = true;
 
@@ -171,6 +188,11 @@
     # Hardware utils
     usbutils
     pciutils
+
+    # Virtualization
+    qemu
+    virt-manager
+    gnome-boxes
 
     # Non-free apps
     vivaldi
