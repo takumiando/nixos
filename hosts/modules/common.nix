f8e2371f635f308e4bfe4573ce72b983c6a22ce8
Takumi Ando <takumi@spacecubics.com>
2025-12-01 10:53:25 +0900

    common: Use noto-fonts-color-emoji instead of noto-fonts-emoji
    The noto-fonts-emoji has been replaced to noto-fonts-color-emoji.
    
    Signed-off-by: Takumi Ando <takumi@spacecubics.com>

--- hosts/modules/common.nix
+++ hosts/modules/common.nix
@@ -52,7 +52,7 @@
     packages = with pkgs; [
       noto-fonts-cjk-serif
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       nerd-fonts.jetbrains-mono
     ];
     fontDir.enable = true;
