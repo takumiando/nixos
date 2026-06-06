4d2632ff53a9dfd37e999e7a4cc5cd4a06cbabc1
Takumi Ando <takumi@spacecubics.com>
2026-06-07 00:23:17 +0900

    home: Run fcitx5 and easyeffects in user session
    Signed-off-by: Takumi Ando <takumi@spacecubics.com>

--- home/takumi.nix
+++ home/takumi.nix
@@ -1,8 +1,29 @@
+{ pkgs, ... }:
+
 {
   home = rec {
     username = "takumi";
     homeDirectory = "/home/${username}";
     stateVersion = "25.11";
   };
+
   programs.home-manager.enable = true;
+
+  services.easyeffects.enable = true;
+
+  systemd.user.services.fcitx5 = {
+    Unit = {
+      Description = "Fcitx 5 input method";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "/run/current-system/sw/bin/fcitx5";
+      Restart = "on-failure";
+      RestartSec = 2;
+    };
+
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
 }
